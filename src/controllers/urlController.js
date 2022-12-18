4bd24830802bb79f6a16029c930660a9e7191a68
Diego Candido <diegocan.sou@gmail.com>
2022-12-17 21:54:04 -0300

    refact: post Url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -5,6 +5,7 @@ const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwyz", 6);
 
 export async function postUrl(req, res) {
   try {
+    const idUser = req.userId
     const { url } = req.url;
     let shortUrl = url;
     shortUrl = nanoid();
@@ -13,6 +14,10 @@ export async function postUrl(req, res) {
       `INSERT INTO urls (url, "shortUrl") VALUES ($1,$2)`,
       [url, shortUrl]
     );
+
+    const id = await connection.query(`SELECT * FROM urls ORDER BY id DESC`)
+
+    await connection.query(`INSERT INTO creates (id_user, id_url) VALUES ($1,$2)`,[idUser, id.rows[0].id])
 
     res.status(201).send({shortUrl})
   } catch (e) {
