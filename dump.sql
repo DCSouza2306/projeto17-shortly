090acde96b2e09fe6ecf20d6ec86187eefcc8bc7
Diego Candido <diegocan.sou@gmail.com>
2022-12-23 12:02:54 -0300

    refact: dump and scripts

--- dump.sql
+++ dump.sql
@@ -145,6 +145,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 --
 
 INSERT INTO public.sessions VALUES (1, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjcxMzk5ODkzLCJleHAiOjE2NzE0ODYyOTN9.nG94PhjWy4b3mBnCLARW9qzvFy7FHcGxNXWDUAk58ys', '2022-12-18 18:44:53.935938');
+INSERT INTO public.sessions VALUES (2, 4, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjcxODA0NDk5LCJleHAiOjE2NzE4OTA4OTl9.FZSXuA-1eGNXAiDINyQr4zUHiBZuUhDStIq5cgzcyQY', '2022-12-23 11:08:19.708604');
 
 
 --
@@ -153,6 +154,9 @@ INSERT INTO public.sessions VALUES (1, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
 
 INSERT INTO public.urls VALUES (1, 1, 'maa5kg', 'https://www.youtube.com/watch?v=3blnRfNEdSM&ab_channel=chillimusic', 3, '2022-12-18 18:47:14.928684');
 INSERT INTO public.urls VALUES (2, 1, 'm213kk', 'https://www.youtube.com/watch?v=9QqatqT1PKM&ab_channel=LofiVibes', 2, '2022-12-18 18:47:42.49978');
+INSERT INTO public.urls VALUES (4, 4, '9j7jh9', 'https://www.youtube.com/watch?v=QpKfPLNHDuY&ab_channel=FalaNerd', 2, '2022-12-23 11:13:48.759138');
+INSERT INTO public.urls VALUES (5, 4, '9ffpkr', 'https://www.youtube.com/watch?v=NdvYI8pnHLI&ab_channel=TioSan', 1, '2022-12-23 11:14:05.81803');
+INSERT INTO public.urls VALUES (8, 4, 'p0p7br', 'https://www.amazon.com.br/Amazfit-GTS-Mini-Modelo-meia-noite/dp/B09Z6GMPC6?ref_=Oct_DLandingS_D_e0f10218_60&th=1', 1, '2022-12-23 11:59:02.600317');
 
 
 --
@@ -161,27 +165,28 @@ INSERT INTO public.urls VALUES (2, 1, 'm213kk', 'https://www.youtube.com/watch?v
 
 INSERT INTO public.users VALUES (1, 'Naruto', 'naruto@email.com', '$2b$10$FSRd9DfYBdZLNvQsbCGpkelQXXH9W3Srk3voBw2JebCZn9FmimKey', '2022-12-18 18:42:31.583611');
 INSERT INTO public.users VALUES (2, 'Diego', 'diego@email.com', '$2b$10$waoZcysCUQqXNdJUGnpdmuIRIHPHqgYdzl3bUJF7QlD865HZjd8yu', '2022-12-18 18:42:55.4682');
+INSERT INTO public.users VALUES (4, 'Uchiha Madara', 'madara.uchiha@email.com', '$2b$10$sbW/EKJFWxhLcWIgZctV6OciNp9wD40jbCwrMJ0pwOtITt6TauXo6', '2022-12-23 11:08:09.438859');
 
 
 --
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 1, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 2, true);
 
 
 --
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 3, true);
+SELECT pg_catalog.setval('public.urls_id_seq', 8, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 2, true);
+SELECT pg_catalog.setval('public.users_id_seq', 4, true);
 
 
 --
